b6acad616530bca915b54717ff35ddd3b0e7dc8b
vmichele <michelet.valentin@gmail.com>
2014-04-20 16:41:45 +0200

    Add a method to get a sphere's bounding box.

--- GeometricObjects/Sphere.cpp
+++ GeometricObjects/Sphere.cpp
@@ -4,57 +4,57 @@
 #include "math.h"
 
 const double Sphere::kEpsilon = 0.001;
-					
+
 // ---------------------------------------------------------------- default constructor
 
-Sphere::Sphere(void)	
-	: 	GeometricObject(),
-		center(0.0),
-		radius(1.0)
+Sphere::Sphere(void)
+  : 	GeometricObject(),
+    center(0.0),
+    radius(1.0)
 {}
 
 
 // ---------------------------------------------------------------- constructor
 
 Sphere::Sphere(Point3D c, double r)
-	: 	GeometricObject(),
-		center(c),
-		radius(r)
+  : 	GeometricObject(),
+    center(c),
+    radius(r)
 {}
 
 
 // ---------------------------------------------------------------- clone
 
-Sphere* 
+Sphere*
 Sphere::clone(void) const {
-	return (new Sphere(*this));
+  return (new Sphere(*this));
 }
 
 
 // ---------------------------------------------------------------- copy constructor
 
 Sphere::Sphere (const Sphere& sphere)
-	: 	GeometricObject(sphere),
-		center(sphere.center),
-		radius(sphere.radius)
+  : 	GeometricObject(sphere),
+    center(sphere.center),
+    radius(sphere.radius)
 {}
 
 
 
 // ---------------------------------------------------------------- assignment operator
 
-Sphere& 
-Sphere::operator= (const Sphere& rhs)		
+Sphere&
+Sphere::operator= (const Sphere& rhs)
 {
-	if (this == &rhs)
-		return (*this);
+  if (this == &rhs)
+    return (*this);
 
-	GeometricObject::operator= (rhs);
+  GeometricObject::operator= (rhs);
 
-	center 	= rhs.center;
-	radius	= rhs.radius;
+  center 	= rhs.center;
+  radius	= rhs.radius;
 
-	return (*this);
+  return (*this);
 }
 
 
@@ -63,42 +63,48 @@ Sphere::operator= (const Sphere& rhs)
 Sphere::~Sphere(void) {}
 
 
+BBox Sphere::get_bounding_box(void) const {
+  return BBox(center.x - radius, center.x + radius,
+              center.y - radius, center.y + radius,
+              center.z - radius, center.z + radius);
+}
+
 //---------------------------------------------------------------- hit
 
 bool
 Sphere::hit(const Ray& ray, double& tmin, ShadeRec& sr) const {
-	double 		t;
-	Vector3D	temp 	= ray.o - center;
-	double 		a 		= ray.d * ray.d;
-	double 		b 		= 2.0 * temp * ray.d;
-	double 		c 		= temp * temp - radius * radius;
-	double 		disc	= b * b - 4.0 * a * c;
-	
-	if (disc < 0.0)
-		return(false);
-	else {	
-		double e = sqrt(disc);
-		double denom = 2.0 * a;
-		t = (-b - e) / denom;    // smaller root
-	
-		if (t > kEpsilon) {
-			tmin = t;
-			sr.normal 	 = (temp + t * ray.d) / radius;
-			sr.local_hit_point = ray.o + t * ray.d;
-			return (true);
-		} 
-	
-		t = (-b + e) / denom;    // larger root
-	
-		if (t > kEpsilon) {
-			tmin = t;
-			sr.normal   = (temp + t * ray.d) / radius;
-			sr.local_hit_point = ray.o + t * ray.d;
-			return (true);
-		} 
-	}
-	
-	return (false);
+  double 		t;
+  Vector3D	temp 	= ray.o - center;
+  double 		a 		= ray.d * ray.d;
+  double 		b 		= 2.0 * temp * ray.d;
+  double 		c 		= temp * temp - radius * radius;
+  double 		disc	= b * b - 4.0 * a * c;
+
+  if (disc < 0.0)
+    return(false);
+  else {
+    double e = sqrt(disc);
+    double denom = 2.0 * a;
+    t = (-b - e) / denom;    // smaller root
+
+    if (t > kEpsilon) {
+      tmin = t;
+      sr.normal 	 = (temp + t * ray.d) / radius;
+      sr.local_hit_point = ray.o + t * ray.d;
+      return (true);
+    }
+
+    t = (-b + e) / denom;    // larger root
+
+    if (t > kEpsilon) {
+      tmin = t;
+      sr.normal   = (temp + t * ray.d) / radius;
+      sr.local_hit_point = ray.o + t * ray.d;
+      return (true);
+    }
+  }
+
+  return (false);
 }
 
 
--- GeometricObjects/Sphere.h
+++ GeometricObjects/Sphere.h
@@ -4,65 +4,68 @@
 // This file contains the declaration of the class Sphere
 
 #include "GeometricObject.h"
+#include "BBox.h"
 
 //-------------------------------------------------------------------------------- class Sphere
 
-class Sphere: public GeometricObject {	
-								  	
-	public:
-		
-		Sphere(void);   									// Default constructor
-				
-		Sphere(Point3D center, double r);					// Constructor 
-					
-		Sphere(const Sphere& sphere); 						// Copy constructor
-		
-		virtual Sphere* 									// Virtual copy constructor
-		clone(void) const;
+class Sphere: public GeometricObject {
 
-		virtual												// Destructor
-		~Sphere(void);   									
+  public:
 
-		Sphere& 											// assignment operator
-		operator= (const Sphere& sphere);				
-																					
-		void
-		set_center(const Point3D& c);
-		
-		void
-		set_center(const double x, const double y, const double z);
-		
-		void
-		set_radius(const double r);
-						
-		virtual bool 												 
-		hit(const Ray& ray, double& t, ShadeRec& s) const;	
-		
-	private:
-	
-		Point3D 	center;   			// center coordinates as a point  
-		double 		radius;				// the radius 
-		
-		static const double kEpsilon;   // for shadows and secondary rays
+    Sphere(void);   									// Default constructor
+
+    Sphere(Point3D center, double r);					// Constructor
+
+    Sphere(const Sphere& sphere); 						// Copy constructor
+
+    virtual Sphere* 									// Virtual copy constructor
+    clone(void) const;
+
+    virtual												// Destructor
+    ~Sphere(void);
+
+    Sphere& 											// assignment operator
+    operator= (const Sphere& sphere);
+
+    void
+    set_center(const Point3D& c);
+
+    void
+    set_center(const double x, const double y, const double z);
+
+    void
+    set_radius(const double r);
+
+    BBox get_bounding_box(void) const;
+
+    virtual bool
+    hit(const Ray& ray, double& t, ShadeRec& s) const;
+
+  private:
+
+    Point3D 	center;   			// center coordinates as a point
+    double 		radius;				// the radius
+
+    static const double kEpsilon;   // for shadows and secondary rays
 };
 
 
 
 inline void
 Sphere::set_center(const Point3D& c) {
-	center = c;
+  center = c;
 }
-		
+
 inline void
 Sphere::set_center(const double x, const double y, const double z) {
-	center.x = x;
-	center.y = y;
-	center.z = z;
+  center.x = x;
+  center.y = y;
+  center.z = z;
 }
-		
+
 inline void
 Sphere::set_radius(const double r) {
-	radius = r;
+  radius = r;
 }
 
 #endif
