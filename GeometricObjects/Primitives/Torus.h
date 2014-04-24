15beaf876a6197f6a8d8e65f6218958e46f26d09
vmichele <michelet.valentin@gmail.com>
2014-04-24 08:47:08 +0200

    Add getter to bounding box in torus object.

--- GeometricObjects/Primitives/Torus.h
+++ GeometricObjects/Primitives/Torus.h
@@ -14,11 +14,16 @@ public:
   Torus& operator=(const Torus& torus);
   Normal compute_normal(const Point3D& p) const;
   virtual bool hit(const Ray& ray, double& t, ShadeRec& s) const;
+  BBox get_bounding_box(void) const;
 
 private:
   double a;
   double b;
   BBox bbox;
 };
+
+inline BBox Torus::get_bounding_box(void) const {
+  return bbox;
+}
 
 #endif // TORUS_H
