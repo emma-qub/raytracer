10b2bc6d14f81ceb16b3115669a86e7ab764fdb1
vmichele <michelet.valentin@gmail.com>
2014-05-18 09:04:50 +0200

    Update build scene to check that rendering is not totally broken.

--- build/BuildShadedObjects.cpp
+++ build/BuildShadedObjects.cpp
@@ -30,106 +30,105 @@
 #include <iostream>
 
 void World::build(void) {
-  int num_samples = 9;
+  int num_samples = 100;
 
-  vp.set_hres(600);
-  vp.set_vres(600);
-  vp.set_samples(num_samples);
-  vp.set_max_depth(5);
+  vp.set_hres(400);
+  vp.set_vres(300);
+  vp.set_pixel_size(0.05);
+  vp.set_sampler(new MultiJittered(num_samples));
+  vp.set_max_depth(0);
 
-  background_color = RGBColor(0.0, 0.3, 0.25);
-
-  tracer_ptr = new Whitted(this);
+  tracer_ptr = new RayCast(this);
+  background_color = white;
 
   Ambient* ambient_ptr = new Ambient;
-  ambient_ptr->scale_radiance(0.25);
+  ambient_ptr->scale_radiance(0.5);
   set_ambient_light(ambient_ptr);
 
-  Pinhole* pinhole_ptr = new Pinhole;
-  pinhole_ptr->set_eye(-8, 5.5, 20);
-  pinhole_ptr->set_lookat(0, 4.5, 0);
-  pinhole_ptr->set_view_distance(1400.0);
-  pinhole_ptr->compute_uvw();
-  set_camera(pinhole_ptr);
+  ThinLens* thin_lens_ptr = new ThinLens;
+  thin_lens_ptr->set_sampler(new MultiJittered(num_samples));
+  thin_lens_ptr->set_eye(0, 6, 50);
+  thin_lens_ptr->set_lookat(0, 6, 0);
+  thin_lens_ptr->set_view_distance(40.0);
+//  thin_lens_ptr->set_focal_distance(50.0);	// for Figure 10.10(a)
+  thin_lens_ptr->set_focal_distance(74.0);	// for Figure 10.10(b)
+//	thin_lens_ptr->set_focal_distance(98.0);	// for Figure 10.10(c)
+  thin_lens_ptr->set_lens_radius(1.0);
+  thin_lens_ptr->compute_uvw();
+  set_camera(thin_lens_ptr);
 
 
-  // point light
-
-  PointLight* light_ptr1 = new PointLight;
-  light_ptr1->set_location(40, 50, 0);
-  light_ptr1->scale_radiance(4.5);
-  light_ptr1->set_shadows(true);
-  add_light(light_ptr1);
+  Directional* light_ptr = new Directional;
+  light_ptr->set_direction(1, 1, 1);
+  light_ptr->scale_radiance(7.5);
+  light_ptr->set_shadows(true);
+  add_light(light_ptr);
 
 
-  // point light
+  // box 1
 
-  PointLight* light_ptr2 = new PointLight;
-  light_ptr2->set_location(-10, 20, 10);
-  light_ptr2->scale_radiance(4.5);
-  light_ptr2->set_shadows(true);
-  add_light(light_ptr2);
+//  Checker3D* checker_ptr1 = new Checker3D;
+//  checker_ptr1->set_size(2.0);
+//  checker_ptr1->set_color1(1, 1, 0.33);  		// lemon
+//  checker_ptr1->set_color2(black);
 
+  /*SV_*/Matte* /*SV_*/Matte_ptr1 = new /*SV_*/Matte;
+  /*SV_*/Matte_ptr1->set_ka(0.5);
+  /*SV_*/Matte_ptr1->set_kd(0.35);
+  /*SV_*/Matte_ptr1->set_cd(1, 1, 0.33);
 
-  // directional light
-
-  Directional* light_ptr3 = new Directional;
-  light_ptr3->set_direction(-1, 0, 0);
-  light_ptr3->scale_radiance(4.5);
-  light_ptr3->set_shadows(true);
-  add_light(light_ptr3);
-
-
-  // transparent torus
-
-  Transparent* glass_ptr = new Transparent;
-  glass_ptr->set_ks(0.2);
-  glass_ptr->set_exp(2000.0);
-  glass_ptr->set_ior(1.5);
-  glass_ptr->set_kr(0.1);
-  glass_ptr->set_kt(0.9);
-
-  Instance* torus_ptr2 = new Instance (new Torus(3.0, 0.8));
-  torus_ptr2->set_material(glass_ptr);
-  torus_ptr2->rotate_x(90);
-  torus_ptr2->rotate_y(20);
-  torus_ptr2->translate(0, 4.5, 0);
-  add_object(torus_ptr2);
+  Box* box_ptr1 = new Box(Point3D(-9, 0, -1), Point3D(-3, 12, 0));
+  box_ptr1->set_material(Matte_ptr1);
+  add_object(box_ptr1);
 
 
-  // red sphere
+  // box 2
 
-  Reflective*	reflective_ptr = new Reflective;
-  reflective_ptr->set_ka(0.3);
-  reflective_ptr->set_kd(0.3);
-  reflective_ptr->set_cd(red);
-  reflective_ptr->set_ks(0.2);
-  reflective_ptr->set_exp(2000.0);
-  reflective_ptr->set_kr(0.25);
+//  Checker3D* checker_ptr2 = new Checker3D;
+//  checker_ptr2->set_size(2.0);
+//  checker_ptr2->set_color1(black);
+//  checker_ptr2->set_color2(0.1, 1, 0.5);	  	// green
 
-  Sphere* sphere_ptr2 = new Sphere(Point3D(4, 4, -6), 3);
-  sphere_ptr2->set_material(reflective_ptr);
-  add_object(sphere_ptr2);
+  /*SV_*/Matte* /*SV_*/Matte_ptr2 = new /*SV_*/Matte;
+  /*SV_*/Matte_ptr2->set_ka(0.5);
+  /*SV_*/Matte_ptr2->set_kd(0.35);
+  /*SV_*/Matte_ptr2->set_cd(0.1, 1, 0.5);
 
+  Box* box_ptr2 = new Box(Point3D(-3.25, 0, -25), Point3D(4.75, 14, -24));
+  box_ptr2->set_material(/*SV_*/Matte_ptr2);
+  add_object(box_ptr2);
+
+
+  // box 3
+
+//  Checker3D* checker_ptr3 = new Checker3D;
+//  checker_ptr3->set_size(2.0);
+//  checker_ptr3->set_color1(black);
+//  checker_ptr3->set_color2(1, 0.6, 0.15);	  	// orange
+
+  /*SV_*/Matte* /*SV_*/Matte_ptr3 = new /*SV_*/Matte;
+  /*SV_*/Matte_ptr3->set_ka(0.5);
+  /*SV_*/Matte_ptr3->set_kd(0.35);
+  /*SV_*/Matte_ptr3->set_cd(1, 0.6, 0.15);
+
+  Box* box_ptr3 = new Box(Point3D(8, 0, -49), Point3D(18, 15, -48));
+  box_ptr3->set_material(Matte_ptr3);
+  add_object(box_ptr3);
+
+
+  // ground plane
 
 //  Checker3D* checker_ptr = new Checker3D;
-//  checker_ptr->set_size(4.0);
-//  checker_ptr->set_color1(0.75);
+//  checker_ptr->set_size(8.0);
+//  checker_ptr->set_color1(0.25);  			// gray
 //  checker_ptr->set_color2(white);
 
-  /*SV_*/Matte* sv_matte_ptr = new /*SV_*/Matte;
-  sv_matte_ptr->set_ka(0.5);
-  sv_matte_ptr->set_kd(0.35);
-  sv_matte_ptr->set_cd(0.75);
+  /*SV_*/Matte* /*SV_*/Matte_ptr = new /*SV_*/Matte;
+  /*SV_*/Matte_ptr->set_ka(0.5);
+  /*SV_*/Matte_ptr->set_kd(0.35);
+  /*SV_*/Matte_ptr->set_cd(0.25);
 
-
-  // rectangle
-
-  Point3D p0(-20, 0, -100);
-  Vector3D a(0, 0, 120);
-  Vector3D b(40, 0, 0);
-
-  Rectangle* rectangle_ptr = new Rectangle(p0, a, b);
-  rectangle_ptr->set_material(sv_matte_ptr);
-  add_object(rectangle_ptr);
+  Plane* plane_ptr1 = new Plane(Point3D(0.0), Normal(0, 1, 0));
+  plane_ptr1->set_material(Matte_ptr);
+  add_object(plane_ptr1);
 }
