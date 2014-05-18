c2d998985da4ccdddad3c71be68061cc01c6f2c3
vmichele <michelet.valentin@gmail.com>
2014-05-18 10:44:28 +0200

    Add a trace ray method in Whitted tracer. I didn't realized that I had to keep the old one and create a new one with another parameter, so I just merged both of them and end up with black images.

--- Tracers/Whitted.cpp
+++ Tracers/Whitted.cpp
@@ -29,6 +29,22 @@ Whitted::~Whitted(void) {
 }
 
 
+RGBColor Whitted::trace_ray(const Ray ray, const int depth) const {
+  if (depth > world_ptr->vp.max_depth)
+    return(black);
+  else {
+    ShadeRec sr(world_ptr->hit_objects(ray));
+
+    if (sr.hit_an_object) {
+      sr.depth = depth;
+      sr.ray = ray;
+      return (sr.material_ptr->shade(sr));
+    }
+    else
+      return (world_ptr->background_color);
+  }
+}
+
 // -------------------------------------------------------------------- trace_ray
 
 RGBColor Whitted::trace_ray(const Ray ray, float& tmin, const int depth) const {
--- build/BuildShadedObjects.cpp
+++ build/BuildShadedObjects.cpp
@@ -30,105 +30,127 @@
 #include <iostream>
 
 void World::build(void) {
-  int num_samples = 100;
+  int num_samples = 16;
 
-  vp.set_hres(400);
-  vp.set_vres(300);
-  vp.set_pixel_size(0.05);
-  vp.set_sampler(new MultiJittered(num_samples));
-  vp.set_max_depth(0);
+  vp.set_hres(600);
+  vp.set_vres(400);
+  vp.set_samples(num_samples);
+//	vp.set_max_depth(0);			// for Figure 24.6(a)
+  vp.set_max_depth(1);			// for Figure 24.6(b)
 
-  tracer_ptr = new RayCast(this);
-  background_color = white;
+  tracer_ptr = new Whitted(this);
+  background_color = RGBColor(0.15);
 
   Ambient* ambient_ptr = new Ambient;
   ambient_ptr->scale_radiance(0.5);
   set_ambient_light(ambient_ptr);
 
-  ThinLens* thin_lens_ptr = new ThinLens;
-  thin_lens_ptr->set_sampler(new MultiJittered(num_samples));
-  thin_lens_ptr->set_eye(0, 6, 50);
-  thin_lens_ptr->set_lookat(0, 6, 0);
-  thin_lens_ptr->set_view_distance(40.0);
-//  thin_lens_ptr->set_focal_distance(50.0);	// for Figure 10.10(a)
-  thin_lens_ptr->set_focal_distance(74.0);	// for Figure 10.10(b)
-//	thin_lens_ptr->set_focal_distance(98.0);	// for Figure 10.10(c)
-  thin_lens_ptr->set_lens_radius(1.0);
-  thin_lens_ptr->compute_uvw();
-  set_camera(thin_lens_ptr);
+
+  Pinhole* pinhole_ptr = new Pinhole;
+  pinhole_ptr->set_eye(75, 40, 100);
+  pinhole_ptr->set_lookat(-10, 39, 0);
+  pinhole_ptr->set_view_distance(360);
+  pinhole_ptr->compute_uvw();
+  set_camera(pinhole_ptr);
 
 
-  Directional* light_ptr = new Directional;
-  light_ptr->set_direction(1, 1, 1);
-  light_ptr->scale_radiance(7.5);
+  PointLight* light_ptr = new PointLight;
+  light_ptr->set_location(150, 150, 0);
+  light_ptr->scale_radiance(3.0);
   light_ptr->set_shadows(true);
   add_light(light_ptr);
 
+  // yellow-green reflective sphere
 
-  // box 1
+  Reflective* reflective_ptr1 = new Reflective;
+  reflective_ptr1->set_ka(0.25);
+  reflective_ptr1->set_kd(0.5);
+  reflective_ptr1->set_cd(0.75, 0.75, 0);    	// yellow
+  reflective_ptr1->set_ks(0.15);
+  reflective_ptr1->set_exp(100.0);
+  reflective_ptr1->set_kr(0.75);
+  reflective_ptr1->set_cr(white); 			// default color
 
-//  Checker3D* checker_ptr1 = new Checker3D;
-//  checker_ptr1->set_size(2.0);
-//  checker_ptr1->set_color1(1, 1, 0.33);  		// lemon
-//  checker_ptr1->set_color2(black);
-
-  /*SV_*/Matte* /*SV_*/Matte_ptr1 = new /*SV_*/Matte;
-  /*SV_*/Matte_ptr1->set_ka(0.5);
-  /*SV_*/Matte_ptr1->set_kd(0.35);
-  /*SV_*/Matte_ptr1->set_cd(1, 1, 0.33);
-
-  Box* box_ptr1 = new Box(Point3D(-9, 0, -1), Point3D(-3, 12, 0));
-  box_ptr1->set_material(Matte_ptr1);
-  add_object(box_ptr1);
-
-
-  // box 2
-
-//  Checker3D* checker_ptr2 = new Checker3D;
-//  checker_ptr2->set_size(2.0);
-//  checker_ptr2->set_color1(black);
-//  checker_ptr2->set_color2(0.1, 1, 0.5);	  	// green
-
-  /*SV_*/Matte* /*SV_*/Matte_ptr2 = new /*SV_*/Matte;
-  /*SV_*/Matte_ptr2->set_ka(0.5);
-  /*SV_*/Matte_ptr2->set_kd(0.35);
-  /*SV_*/Matte_ptr2->set_cd(0.1, 1, 0.5);
-
-  Box* box_ptr2 = new Box(Point3D(-3.25, 0, -25), Point3D(4.75, 14, -24));
-  box_ptr2->set_material(/*SV_*/Matte_ptr2);
-  add_object(box_ptr2);
+  float radius = 23.0;
+  Sphere* sphere_ptr1 = new Sphere(Point3D(38, radius, -25), radius);
+  sphere_ptr1->set_material(reflective_ptr1);
+  add_object(sphere_ptr1);
 
 
-  // box 3
+  // orange non-reflective sphere
 
-//  Checker3D* checker_ptr3 = new Checker3D;
-//  checker_ptr3->set_size(2.0);
-//  checker_ptr3->set_color1(black);
-//  checker_ptr3->set_color2(1, 0.6, 0.15);	  	// orange
+  Matte* matte_ptr1 = new Matte;
+  matte_ptr1->set_ka(0.45);
+  matte_ptr1->set_kd(0.75);
+  matte_ptr1->set_cd(0.75, 0.25, 0);   // orange
 
-  /*SV_*/Matte* /*SV_*/Matte_ptr3 = new /*SV_*/Matte;
-  /*SV_*/Matte_ptr3->set_ka(0.5);
-  /*SV_*/Matte_ptr3->set_kd(0.35);
-  /*SV_*/Matte_ptr3->set_cd(1, 0.6, 0.15);
+  Sphere* sphere_ptr2 = new Sphere(Point3D(-7, 10, 42), 20);
+  sphere_ptr2->set_material(matte_ptr1);
+  add_object(sphere_ptr2);
 
-  Box* box_ptr3 = new Box(Point3D(8, 0, -49), Point3D(18, 15, -48));
-  box_ptr3->set_material(Matte_ptr3);
-  add_object(box_ptr3);
+
+  // sphere on top of box
+
+  Reflective* reflective_ptr2 = new Reflective;
+  reflective_ptr2->set_ka(0.35);
+  reflective_ptr2->set_kd(0.75);
+  reflective_ptr2->set_cd(black);
+  reflective_ptr2->set_ks(0.0);		// default value
+  reflective_ptr2->set_exp(1.0);		// default value, but irrelevant in this case
+  reflective_ptr2->set_kr(0.75);
+  reflective_ptr2->set_cr(white);
+
+  Sphere* sphere_ptr3 = new Sphere(Point3D(-30, 59, 35), 20);
+  sphere_ptr3->set_material(reflective_ptr2);
+  add_object(sphere_ptr3);
+
+
+  // cylinder
+
+  Reflective* reflective_ptr3 = new Reflective;
+  reflective_ptr3->set_ka(0.35);
+  reflective_ptr3->set_kd(0.5);
+  reflective_ptr3->set_cd(0, 0.5, 0.75);   // cyan
+  reflective_ptr3->set_ks(0.2);
+  reflective_ptr3->set_exp(100.0);
+  reflective_ptr3->set_kr(0.75);
+  reflective_ptr3->set_cr(white);
+
+  double bottom 			= 0.0;
+  double top 				= 85;
+  double cylinder_radius	= 22;
+  SolidCylinder* cylinder_ptr = new SolidCylinder(bottom, top, cylinder_radius);
+  cylinder_ptr->set_material(reflective_ptr3);
+  add_object(cylinder_ptr);
+
+
+  // box
+
+  Matte* matte_ptr2 = new Matte;
+  matte_ptr2->set_ka(0.15);
+  matte_ptr2->set_kd(0.5);
+  matte_ptr2->set_cd(0.75, 1.0, 0.75);   // light green
+
+  Box* box_ptr = new Box(Point3D(-35, 0, -110), Point3D(-25, 60, 65));
+  box_ptr->set_material(matte_ptr2);
+  add_object(box_ptr);
 
 
   // ground plane
 
-//  Checker3D* checker_ptr = new Checker3D;
-//  checker_ptr->set_size(8.0);
-//  checker_ptr->set_color1(0.25);  			// gray
+//  PlaneChecker* checker_ptr = new PlaneChecker;
+//  checker_ptr->set_size(20.0);
+//  checker_ptr->set_outline_width(2.0);
+//  checker_ptr->set_color1(white);
 //  checker_ptr->set_color2(white);
+//  checker_ptr->set_outline_color(black);
 
-  /*SV_*/Matte* /*SV_*/Matte_ptr = new /*SV_*/Matte;
-  /*SV_*/Matte_ptr->set_ka(0.5);
-  /*SV_*/Matte_ptr->set_kd(0.35);
-  /*SV_*/Matte_ptr->set_cd(0.25);
+  /*SV_*/Matte* sv_matte_ptr = new /*SV_*/Matte;
+  sv_matte_ptr->set_ka(0.30);
+  sv_matte_ptr->set_kd(0.9);
+  sv_matte_ptr->set_cd(0.25);
 
-  Plane* plane_ptr1 = new Plane(Point3D(0.0), Normal(0, 1, 0));
-  plane_ptr1->set_material(Matte_ptr);
-  add_object(plane_ptr1);
+  Plane* plane_ptr = new Plane(Point3D(0), Normal(0, 1, 0));
+  plane_ptr->set_material(sv_matte_ptr);
+  add_object(plane_ptr);
 }
